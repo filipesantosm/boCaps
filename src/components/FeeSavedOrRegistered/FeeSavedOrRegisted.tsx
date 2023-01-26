671b5bdcd6727e756dc4494abf475ecf296ce159
Ravenpac <ravennapac@gmail.com>
2023-01-26 09:44:37 -0300

    feat: delete picture modal and fee saved and registered modal

---
+++ src/components/FeeSavedOrRegistered/FeeSavedOrRegisted.tsx
@@ -0,0 +1,25 @@
+import { Container, Content, Description, SuccessIcon, Title } from './styles';
+import success from '../../assets/img/success.svg';
+
+interface ModalProps {
+  isOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  text: 'salvo' | 'cadastrado';
+}
+
+const FeeSavedOrDeleted = ({ isOpen, text }: ModalProps) => {
+  return (
+    <Container onClick={() => isOpen(false)}>
+      <Content>
+        <SuccessIcon src={success} alt="Ícone de sucesso" />
+
+        <Title>Sucesso!</Title>
+
+        <Description>
+          Green-fee &quot;nome do green-fee&quot; {text} com sucesso.
+        </Description>
+      </Content>
+    </Container>
+  );
+};
+
+export default FeeSavedOrDeleted;
