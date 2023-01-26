61a670b62f9506fa875ab88aa6cc4fbde479c61b
Ravenpac <ravennapac@gmail.com>
2023-01-26 15:03:21 -0300

    feat: sales list

--- src/pages/Sales/Sales.tsx
+++ src/pages/Sales/Sales.tsx
@@ -1,8 +1,91 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import NavBar from '../../components/NavBar/NavBar';
-import { Container, Content, MainForm } from './styles';
+import SmallPagination from '../../components/Pagination/Pagination';
+import {
+  CompText,
+  Container,
+  Content,
+  GreenFeeComp,
+  MainForm,
+  ManageButton,
+  SearchDivider,
+  SearchIcon,
+  SearchInput,
+  Table,
+  TableHeader,
+  TableHeaderTitle,
+  Title,
+  TitleDivider,
+  TitleIcon,
+} from './styles';
 
 const Sales = () => {
+  const [page, setPage] = useState(1);
+
+  const navigate = useNavigate();
+
+  // mock
+  const sales = [
+    {
+      id: 1,
+      name: 'Atenas Golf Club',
+    },
+    {
+      id: 2,
+      name: 'Country Golf Club',
+    },
+    {
+      id: 3,
+      name: 'Clube Golf Diamantina',
+    },
+    {
+      id: 4,
+      name: 'Jundiaí Club',
+    },
+    {
+      id: 5,
+      name: 'K Golf Club',
+    },
+    {
+      id: 6,
+      name: 'L Golf Club',
+    },
+    {
+      id: 7,
+      name: 'M Golf Club',
+    },
+    {
+      id: 8,
+      name: 'N Golf Club',
+    },
+    {
+      id: 9,
+      name: 'O Golf Club',
+    },
+    {
+      id: 10,
+      name: 'P Golf Club',
+    },
+    {
+      id: 11,
+      name: 'Q Golf Club',
+    },
+    {
+      id: 12,
+      name: 'R Golf Club',
+    },
+    {
+      id: 13,
+      name: 'S Golf Club',
+    },
+    {
+      id: 14,
+      name: 'T Golf Club',
+    },
+  ];
+
   return (
     <Container>
       <NavBar />
@@ -10,7 +93,55 @@ const Sales = () => {
       <Content>
         <Header />
 
-        <MainForm />
+        <MainForm>
+          <TitleDivider>
+            <TitleIcon />
+
+            <Title>Promoções</Title>
+          </TitleDivider>
+
+          <ManageButton type="button" onClick={() => navigate('/sales/manage')}>
+            Gerenciar promoções padrão
+          </ManageButton>
+
+          <TableHeader>
+            <TableHeaderTitle style={{ marginRight: '2.375rem' }}>
+              N°
+            </TableHeaderTitle>
+
+            <TableHeaderTitle style={{ marginRight: '1.625rem' }}>
+              Nome
+            </TableHeaderTitle>
+
+            <SearchDivider>
+              <SearchIcon />
+
+              <SearchInput
+                type="text"
+                id="search"
+                name="search"
+                placeholder="Buscar promoção"
+              />
+            </SearchDivider>
+          </TableHeader>
+
+          <Table>
+            {sales.length > 0 &&
+              sales.map(sale => (
+                <GreenFeeComp key={sale.id}>
+                  <CompText style={{ width: '4.375rem' }}>{sale.id}</CompText>
+
+                  <CompText>{sale.name}</CompText>
+                </GreenFeeComp>
+              ))}
+          </Table>
+
+          <SmallPagination
+            total={5}
+            currentPage={page}
+            handleChange={() => setPage(page + 1)}
+          />
+        </MainForm>
       </Content>
     </Container>
   );
--- src/pages/Sales/styles.ts
+++ src/pages/Sales/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { TbDiscount2 } from 'react-icons/tb';
+import { BiSearch } from 'react-icons/bi';
 
 export const Container = styled.div`
   width: 100%;
@@ -31,4 +33,172 @@ export const MainForm = styled.main`
   flex-direction: column;
 
   padding-top: 2.125rem;
+`;
+
+export const TitleDivider = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+
+  padding-left: 2.5rem;
+
+  margin-bottom: 4rem;
+`;
+
+export const TitleIcon = styled(TbDiscount2)`
+  font-size: 1.875rem;
+
+  color: #003575;
+`;
+
+export const Title = styled.span`
+  font-family: 'Hind Siliguri';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 3rem;
+
+  color: #007126;
+`;
+
+export const ManageButton = styled.button`
+  width: 22.625rem;
+  height: 4.625rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background: transparent;
+
+  border: 1px solid #003575;
+  border-radius: 0.75rem;
+
+  font-family: 'Hind Siliguri';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 1.25rem;
+
+  color: #003575;
+
+  transition: 500ms;
+
+  margin-bottom: 4rem;
+
+  &:hover {
+    background: #003575;
+    color: #fff;
+  }
+`;
+
+export const TableHeader = styled.div`
+  width: 100%;
+  height: 2.75rem;
+
+  display: flex;
+  align-items: center;
+
+  padding-left: 2.5rem;
+
+  margin-bottom: 2rem;
+`;
+
+export const TableHeaderTitle = styled.span`
+  font-family: 'Hind Siliguri';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 1.75rem;
+
+  color: #007126;
+`;
+
+export const SearchDivider = styled.div`
+  width: 14.625rem;
+  height: 2.75rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  position: relative;
+`;
+
+export const SearchInput = styled.input`
+  width: 100%;
+  height: 2.75rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  outline: none;
+
+  background: transparent;
+
+  padding-left: 2.875rem;
+
+  box-shadow: 0px 2px 5px rgba(175, 176, 197, 0.42);
+
+  border: none;
+  border-radius: 0.75rem;
+
+  font-family: 'Hind Siliguri';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 0.875rem;
+
+  font-feature-settings: 'pnum' on, 'lnum' on;
+
+  color: #818da3;
+`;
+
+export const SearchIcon = styled(BiSearch)`
+  position: absolute;
+  align-self: center;
+  left: 1rem;
+
+  color: #c6cedd;
+
+  font-size: 1.5rem;
+`;
+
+export const Table = styled.div`
+  width: 100%;
+  height: calc(100% - 26.375rem);
+
+  padding-right: 3.5rem;
+
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 2.75rem;
+  grid-template-rows: repeat(7, 1fr);
+  grid-row-gap: 0.625rem;
+
+  margin-bottom: auto;
+`;
+
+export const GreenFeeComp = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  padding-left: 2.5rem;
+
+  background: #ffffff;
+
+  box-shadow: 0px 2px 5px rgba(175, 176, 197, 0.42);
+
+  border: none;
+  border-radius: 1.5rem;
+`;
+
+export const CompText = styled.span`
+  font-family: 'Hind Siliguri';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1rem;
+
+  color: #515258;
 `;
