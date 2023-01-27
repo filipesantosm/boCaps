728cd3997dd7fb3a90bab2b72d25e377800e7122
Ravenpac <ravennapac@gmail.com>
2023-01-27 09:41:29 -0300

    fix: terms height

--- src/pages/Terms/styles.ts
+++ src/pages/Terms/styles.ts
@@ -4,26 +4,33 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
+
   display: flex;
   align-items: flex-start;
   justify-content: flex-start;
-  padding: 2.125rem 2.25rem 2.25rem 2rem;
+
+  padding: 1.875rem 1.875rem 2.5rem 1rem;
+
   position: relative;
 `;
 
 export const Content = styled.div`
   width: calc(100% - 14.75rem);
   height: 100%;
+
   display: flex;
   flex-direction: column;
-  padding: 0.125rem 0 0 6rem;
+
+  padding: 0.125rem 0 0 4.125rem;
 `;
 
 export const MainForm = styled.main`
   width: 100%;
   height: calc(100% - 5rem);
+
   display: flex;
   flex-direction: column;
+
   padding-top: 2.125rem;
 `;
 
@@ -31,7 +38,7 @@ export const TitleDivider = styled.div`
   display: flex;
   align-items: center;
   gap: 2.625rem;
-  margin-bottom: 5rem;
+  margin-bottom: 2rem;
 `;
 
 export const TitleIcon = styled(TbClipboardText)`
@@ -54,7 +61,7 @@ export const Title = styled.span`
 
 export const TextArea = styled.textarea`
   width: calc(100% - 1.5rem);
-  height: 23rem;
+  height: calc(100% - 15rem);
   padding: 2.125rem 3.25rem 1.875rem 2.5rem;
   outline: none;
   resize: none;
