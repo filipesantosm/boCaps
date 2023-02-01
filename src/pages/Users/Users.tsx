835617b3db2a747afcc02ff02138435132c220c1
Ravenpac <ravennapac@gmail.com>
2023-02-01 16:12:46 -0300

    feat: user club page

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,19 +1,36 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import NavBar from '../../components/NavBar/NavBar';
+import SmallPagination from '../../components/Pagination/Pagination';
 import {
+  CompDivider,
+  CompText,
   Container,
   Content,
+  DeleteIcon,
   FilterDivider,
   FilterText,
   MainForm,
+  SearchDivider,
+  SearchIcon,
+  SearchInput,
+  TableBody,
+  TableHeader,
+  TableHeaderDivider,
   Title,
   TitleDivider,
   TitleIcon,
+  UserComp,
+  VisualizeIcon,
 } from './styles';
 
 const Users = () => {
   const [tab, setTab] = useState('clubs');
+  const [clubPage, setClubPage] = useState(1);
+  const [clientPage, setClientPage] = useState(1);
+
+  const navigate = useNavigate();
 
   return (
     <Container>
@@ -44,6 +61,60 @@ const Users = () => {
               Clientes
             </FilterText>
           </FilterDivider>
+
+          {tab === 'clubs' && (
+            <>
+              <TableHeader>
+                <TableHeaderDivider>N°</TableHeaderDivider>
+
+                <TableHeaderDivider>
+                  Nome
+                  <SearchDivider>
+                    <SearchIcon />
+
+                    <SearchInput
+                      type="text"
+                      id="search"
+                      name="search"
+                      placeholder="Buscar clube"
+                    />
+                  </SearchDivider>
+                </TableHeaderDivider>
+
+                <TableHeaderDivider>Visualizar</TableHeaderDivider>
+
+                <TableHeaderDivider />
+              </TableHeader>
+
+              <TableBody>
+                <UserComp>
+                  <CompDivider>
+                    <CompText>1</CompText>
+                  </CompDivider>
+
+                  <CompDivider>
+                    <CompText>Atenas Golf Club</CompText>
+                  </CompDivider>
+
+                  <CompDivider>
+                    <VisualizeIcon
+                      onClick={() => navigate('/users/club/001')}
+                    />
+                  </CompDivider>
+
+                  <CompDivider>
+                    <DeleteIcon />
+                  </CompDivider>
+                </UserComp>
+              </TableBody>
+
+              <SmallPagination
+                total={5}
+                currentPage={clubPage}
+                handleChange={() => setClubPage(clubPage + 1)}
+              />
+            </>
+          )}
         </MainForm>
       </Content>
     </Container>
--- src/pages/Users/styles.ts
+++ src/pages/Users/styles.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { HiOutlineUsers } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi';
+import { AiOutlineEye } from 'react-icons/ai';
+import { FiTrash } from 'react-icons/fi';
 
 export const Container = styled.div`
   width: 100%;
@@ -110,5 +112,191 @@ export const FilterText = styled.span<FilterProps>`
     background: #00182c;
 
     visibility: ${props => (props.isSelected ? 'visible' : 'hidden')};
+  }
+`;
+
+export const TableHeader = styled.div`
+  width: 100%;
+  height: fit-content;
+
+  display: grid;
+  grid-template-columns: 1fr 8fr 2fr 1fr;
+
+  padding-right: 3.125rem;
+
+  margin-bottom: 2.375rem;
+`;
+
+export const TableHeaderDivider = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  font-family: 'Hind Siliguri';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 1.75rem;
+
+  color: #007126;
+
+  &:nth-of-type(1) {
+    padding-left: 2.5rem;
+  }
+
+  &:nth-of-type(2) {
+    gap: 1.625rem;
+  }
+
+  &:nth-of-type(3) {
+    justify-content: center;
+  }
+
+  &:nth-of-type(4) {
+    justify-content: flex-end;
+
+    padding-right: 1.375rem;
+  }
+`;
+
+export const SearchDivider = styled.div`
+  width: 14.625rem;
+  height: 2.75rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  position: relative;
+`;
+
+export const SearchInput = styled.input`
+  width: 100%;
+  height: 2.75rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  outline: none;
+
+  background: transparent;
+
+  padding-left: 2.875rem;
+
+  box-shadow: 0px 2px 5px rgba(175, 176, 197, 0.42);
+
+  border: none;
+  border-radius: 0.75rem;
+
+  font-family: 'Hind Siliguri';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 0.875rem;
+
+  font-feature-settings: 'pnum' on, 'lnum' on;
+
+  color: #818da3;
+`;
+
+export const SearchIcon = styled(BiSearch)`
+  position: absolute;
+  align-self: center;
+  left: 1rem;
+
+  color: #c6cedd;
+
+  font-size: 1.5rem;
+`;
+
+export const TableBody = styled.div`
+  width: 100%;
+  height: calc(100% - 26.625rem);
+
+  padding-right: 3.125rem;
+
+  display: grid;
+  grid-template-rows: repeat(6, 1fr);
+  grid-row-gap: 0.625rem;
+
+  margin-bottom: auto;
+`;
+
+export const UserComp = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: grid;
+  grid-template-columns: 1fr 8fr 2fr 1fr;
+
+  background: #ffffff;
+
+  box-shadow: 0px 2px 5px rgba(175, 176, 197, 0.42);
+
+  border: none;
+  border-radius: 1.5rem;
+`;
+
+export const CompDivider = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  font-family: 'Hind Siliguri';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1rem;
+
+  color: #515258;
+
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+
+  &:nth-of-type(1) {
+    padding-left: 2.5rem;
+  }
+
+  &:nth-of-type(3) {
+    justify-content: center;
+  }
+
+  &:nth-of-type(4) {
+    justify-content: flex-end;
+
+    padding-right: 1.375rem;
+  }
+`;
+
+export const CompText = styled.div`
+  display: block;
+
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+`;
+
+export const VisualizeIcon = styled(AiOutlineEye)`
+  font-size: 1.75rem;
+
+  color: #007126;
+
+  transition: 500ms;
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.75;
+  }
+`;
+
+export const DeleteIcon = styled(FiTrash)`
+  font-size: 1.5rem;
+
+  color: #003575;
+
+  transition: 500ms;
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.75;
   }
 `;
