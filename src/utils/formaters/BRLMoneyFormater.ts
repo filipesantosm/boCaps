faa505d3dceec83e6f3035ea176ac3d5ad0138a0
EnzoWu479 <enwu2014@hotmail.com>
2023-08-04 17:23:56 -0300

    feat: added formaters

---
+++ src/utils/formaters/BRLMoneyFormater.ts
@@ -0,0 +1,5 @@
+const BRLMoneyFormater = new Intl.NumberFormat('pt-br', {
+  style: 'currency',
+  currency: 'BRL',
+});
+export default BRLMoneyFormater;
---
+++ src/validations/SweepstakeSchema.ts
@@ -0,0 +1,5 @@
+import * as yup from 'yup';
+
+const SweepstakeSchema = yup.object().shape({});
+
+export default SweepstakeSchema;
