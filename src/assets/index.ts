f9d30c0803474ee61059654d817e28b902f655f8
EnzoWu479 <enwu2014@hotmail.com>
2023-08-04 17:18:26 -0300

    feat: assets added

---
+++ src/assets/index.ts
@@ -0,0 +1,3 @@
+export { default as icon_ArrowSection } from './img/arrowSection.svg';
+export { default as img_placeholderImg } from './img/imgPlaceholderImg.png';
+export { default as img_placeholderPDF } from './img/imgPlaceholderPDF.png';
